99a15b499b998bfcebde55fd013902507de18286
github username <github email address>
2020-05-30 16:37:25 -0700

    user model method fixed to  use the initial weight from signup when they initially signup to show the difference from their goal.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     end
 
     def difference_from_goal_weight
-        (self.goal - last_recorded_weight.weight_input).abs()
+        last_recorded_weight ? (self.goal - last_recorded_weight.weight_input).abs() : (self.goal - self.weight).abs()
     end
 
     def last_recorded_weight
