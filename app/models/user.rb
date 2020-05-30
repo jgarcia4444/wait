fb4ba17998f794d11d3a9bee2779dda1d93c8d25
github username <github email address>
2020-05-30 11:19:22 -0700

    User is able to see the disstance from their goal.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :weights
+
+    def weight_entries_count
+        self.weights.count
+    end
+
+    def difference_from_goal_weight
+        (self.goal - last_recorded_weight.weight_input).abs()
+    end
+
+    def last_recorded_weight
+        self.weights.last
+    end
+
 end
