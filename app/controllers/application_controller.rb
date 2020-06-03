65a86e3fcd5218f0fcf3e052d6b148089d0a3f8d
github username <github email address>
2020-06-02 21:11:58 -0700

    User is greeted with a time dependent greeting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+
+    helper_method :current_user, :time_dependent_greeting
+
     def current_user
         User.find_by(:id => session[:user_id])
     end
+
+    def time_dependent_greeting
+        hour_of_the_day = Time.now.hour
+        if hour_of_the_day < 12
+            "Good Morning"
+        elsif hour_of_the_day > 12 && hour_of_the_day < 17
+            "Good Afternoon"
+        else
+            "Good Evening"
+        end
+    end
+
 end
